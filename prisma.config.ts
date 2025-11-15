920f377f1fdb0dec75648c9d9f02177afa025124
PREM015 <152625984+PREM015@users.noreply.github.com>
2025-11-15 16:58:49 +0530

    prisma add

---
+++ prisma.config.ts
@@ -0,0 +1,9 @@
+import "dotenv/config"; // <-- REQUIRED so Prisma loads .env automatically
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+});
